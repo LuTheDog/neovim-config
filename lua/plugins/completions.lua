406af26b11ca5d38d5dbb6da592a1d37548731db
LuTheDog <lukamatalzic@gmail.com>
2024-04-12 15:46:51 +0200

    Format files and change cmp keybinds, fix relative lines with gitsigns

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,25 +1,25 @@
 return {
-    {
-        "echasnovski/mini.pairs",
+	{
+		"echasnovski/mini.pairs",
 
-        event = "VeryLazy",
-        opts = {},
-        keys = {
-            {
-                "<leader>up",
-                function()
-                    vim.g.minipairs_disable = not vim.g.minipairs_disable
-                    -- if vim.g.minipairs_disable then
-                    --     LazyVim.warn("Disabled auto pairs", { title = "Option" })
-                    -- else
-                    --     LazyVim.info("Enabled auto pairs", { title = "Option" })
-                    -- end
-                end,
-                desc = "Toggle Auto Pairs",
-            },
-        },
-    },
-    {
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"<leader>up",
+				function()
+					vim.g.minipairs_disable = not vim.g.minipairs_disable
+					-- if vim.g.minipairs_disable then
+					--     LazyVim.warn("Disabled auto pairs", { title = "Option" })
+					-- else
+					--     LazyVim.info("Enabled auto pairs", { title = "Option" })
+					-- end
+				end,
+				desc = "Toggle Auto Pairs",
+			},
+		},
+	},
+	{
 		"hrsh7th/cmp-nvim-lsp",
 	},
 	{
@@ -31,6 +31,10 @@ return {
 	},
 	{
 		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+		},
 		config = function()
 			-- Set up nvim-cmp.
 			local cmp = require("cmp")
@@ -40,11 +44,7 @@ return {
 				snippet = {
 					-- REQUIRED - you must specify a snippet engine
 					expand = function(args)
-						-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
 						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-						-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-						-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-						-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
 					end,
 				},
 				window = {
@@ -52,17 +52,17 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4), -- it was C-b
-					["<C-f>"] = cmp.mapping.scroll_docs(4), -- it was C-f
-					["<C-j>"] = cmp.mapping.select_next_item(),
-					["<C-k>"] = cmp.mapping.select_prev_item(),
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<TAB>"] = cmp.mapping.select_next_item(),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "path" },
+
 					{ name = "luasnip" }, -- For luasnip users.
 				}, {
 					{ name = "buffer" },
